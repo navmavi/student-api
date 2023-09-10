74784335445811122bf00e313b656dd2a25b6d73
Gurinder Singh <gurinder.rawala@fluxninja.com>
2023-09-09 23:43:03 -0400

    added prettier

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,11 @@
-const{ Router }= require("express");
-const controller=require('./controller')
-const router= Router();
+const { Router } = require('express');
+const controller = require('./controller');
+const router = Router();
 
-router.get("/", controller.getStudent);
-router.post("/", controller.addStudent);
-router.get("/:id", controller.getStudentById);
-router.put("/:id", controller.updateStudent);
-router.delete("/:id",controller.removeStudent);
+router.get('/', controller.getStudent);
+router.post('/', controller.addStudent);
+router.get('/:id', controller.getStudentById);
+router.put('/:id', controller.updateStudent);
+router.delete('/:id', controller.removeStudent);
 
-  module.exports = router;
-
+module.exports = router;
